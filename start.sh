bd0d864713ef68b6c5e28c614325394da7362a6a
wsp-123 <arpusa2@outlook.com>
2025-09-07 15:35:22 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "🚀 Pulling the latest image from Docker Hub..."
 docker pull arpusa2/ubuntu-2510term:latest
 
 echo "📦 Starting the container..."
-docker run -dit --name ubuntu2510term arpusa2/ubuntu-2510-term:latest
+docker run -dit --name ubuntu2510term arpusa2/ubuntu-2510term:latest
 
 echo "🖥️ Attaching to the container terminal..."
 docker exec -it ubuntu2510-term bash
